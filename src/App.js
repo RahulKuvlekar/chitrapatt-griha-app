3d9b0bb2a86f58c71ad387932c5aaea7cff44c71
RAHUL KUVLEKAR <75781543+RahulKuvlekar@users.noreply.github.com>
2021-10-23 23:55:04 +0530

    Style- Updated Styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./App.css";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Trending from "./Pages/Trending/Trending";
+import Movies from "./Pages/Movies/Movies";
+import TVseries from "./Pages/TVseries/TVseries";
+import Search from "./Pages/Search/Search";
+import { Container } from "@mui/material";
 
 function App() {
   return (
-    <div className="app">
-      <h1>चित्रपट गृह 🎉</h1>
-    </div>
+    <Router>
+      <Header />
+      <div className="app">
+        <Container>
+          <Switch>
+            <Route exact path="/" component={Trending} />
+            <Route exact path="/movies" component={Movies} />
+            <Route path="/tv-series" component={TVseries} />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </Container>
+      </div>
+      <Footer />
+    </Router>
   );
 }
 
